213f2d2aee16c59bb82b44f47e8fa0b881c5c31b
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-04-08 15:26:54 +0530

    deploy.sh: Update themes before building

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
+cd themes
+for Dir in $(ls)
+do 
+    echo "Updating theme $Dir"
+    cd $Dir
+	git pull --ff-only origin master
+	cd ..
+done
+cd ..
+
+echo [Theme Updating complete]
 echo "[building]"
 hugo -t <theme> # Replace with name of the theme
+
 echo "[Pushing to master]"
 cd public/
 git checkout master
